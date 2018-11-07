d38da8040253458365eaad78b7df0721e31ffedf
metamn <bartus.csongor@gmail.com>
2018-11-07 11:32:44 +0200

    Working on the admin menu

--- includes/Test/Plugin/Features/AdminTestMenu.php
+++ includes/Test/Plugin/Features/AdminTestMenu.php
@@ -43,6 +43,7 @@ if ( ! class_exists( 'Test_Plugin_Features_AdminTestMenu' ) ) {
 		 */
 		public function activate() {
 			add_action( 'admin_init', $this->add_menu_item() );
+			add_action( 'admin_init', $this->add_new_setting() );
 		}
 
 		/**
@@ -81,6 +82,25 @@ if ( ! class_exists( 'Test_Plugin_Features_AdminTestMenu' ) ) {
 			remove_menu_page( 'wp_test_menu' );
 		}
 
+		public function add_new_setting() {
+			register_setting( 'wp_test_menu', 'wp_test_menu_list_setting' );
+
+			add_settings_section(
+				'wp_test_menu_settings_section',
+				'WP Test Menu Settings Section',
+				array( $this, 'display_menu_page_header' ),
+				'wp_test_menu'
+			);
+
+			add_settings_field(
+				'wp_test_menu_settings_field',
+				'WP Test Menu Settings Field',
+				array( $this, 'display_menu_page_field' ),
+				'wp_test_menu',
+				'wp_test_menu_settings_section'
+			);
+		}
+
 		/**
 		 * Displays the content of the admin menu
 		 * 
@@ -88,7 +108,38 @@ if ( ! class_exists( 'Test_Plugin_Features_AdminTestMenu' ) ) {
 		 * @return void
 		 */
 		public function display_menu_page() {
-			echo 'xxx';
+			?>
+			<form action='options.php' method='post'>
+				<h2>WP Test Menu</h2>
+				<?php
+					settings_fields( 'wp_test_menu' );
+					do_settings_sections( 'wp_test_menu' );
+					submit_button();
+				?>
+			</form>
+			<?php
+		}
+
+		public function display_menu_page_header() {
+			echo '<h1>WP Test Menu</h1>';
+		}
+
+		/**
+		 * Displays a form on the admin
+		 * 
+		 * @since 1.0.0
+		 * @return string
+		 */
+		public function display_menu_page_field() {
+			$ret = '';
+
+			$ret .= '<select id="list" name="list">';
+			$ret .= '<option value="list1>List1</option>';
+			$ret .= '<option value="list2>List2</option>';
+			$ret .= '<option value="list3>List3</option>';
+			$ret .= '</select>';
+
+			echo $ret;
 		}
 	}
 } // End if().
